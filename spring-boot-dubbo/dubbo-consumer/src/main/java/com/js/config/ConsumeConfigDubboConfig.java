52f427c731de4b4aa8f530e62218a95b0b78ea5c
姜爽 <jjiangshuang@gmail.com>
2021-02-24 16:54:01 +0800

    处理dubbo的20880端口冲突

---
+++ spring-boot-dubbo/dubbo-consumer/src/main/java/com/js/config/ConsumeConfigDubboConfig.java
@@ -0,0 +1,17 @@
+//package com.js.config;
+//
+//import org.apache.dubbo.config.ConsumerConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ConsumeConfigDubboConfig {
+//    @Bean
+//    public ConsumerConfig consumerConfig() {
+//        ConsumerConfig consumerConfig = new ConsumerConfig();
+//        consumerConfig.setCheck(false);
+//        consumerConfig.setTimeout(20000);
+//        return consumerConfig;
+//    }
+//}
+//
