2c8dabeedf5bc6a5edeee25209af7b7392a7e702
姜爽 <2545251075@qq.com>
2021-12-24 19:11:22 +0800

    fix

--- spring-boot-dubbo/dubbo-provider/src/main/java/com/js/service/DemoServiceImpl.java
+++ spring-boot-dubbo/dubbo-provider/src/main/java/com/js/service/DemoServiceImpl.java
@@ -2,14 +2,19 @@ package com.js.service;
 
 import com.js.api.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class DemoServiceImpl implements DemoService {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @Override
     public String sayHello(String name) {
+        log.info("当前的端口号为{}", port);
         log.info("当前服务被申请访问");
         return "Hello " + name;
     }
