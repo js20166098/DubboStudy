926ebad65d118e2a46ae40d18584c9002541fd73
姜爽 <jiangshuang@wejoydata.com>
2021-12-24 12:37:16 +0800

    fix dubbo 3.4

--- spring-boot-dubbo/dubbo-provider/src/main/java/com/js/service/DemoServiceImpl.java
+++ spring-boot-dubbo/dubbo-provider/src/main/java/com/js/service/DemoServiceImpl.java
@@ -1,13 +1,16 @@
 package com.js.service;
 
 import com.js.api.DemoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public String sayHello(String name) {
+        log.info("当前服务被申请访问");
         return "Hello " + name;
     }
 }
