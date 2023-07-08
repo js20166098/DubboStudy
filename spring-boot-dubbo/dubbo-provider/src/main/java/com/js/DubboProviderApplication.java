9bdede9b022a575e6d6af6ef8388f6b2a6c019c6
dujie <jjiangshuang@gmail.com>
2023-07-09 07:43:10 +0800

    dubbo 相关链路追踪

--- spring-boot-dubbo/dubbo-provider/src/main/java/com/js/DubboProviderApplication.java
+++ spring-boot-dubbo/dubbo-provider/src/main/java/com/js/DubboProviderApplication.java
@@ -2,10 +2,12 @@ package com.js;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("classpath:dubbo/*.xml")
+@ComponentScan(basePackages = {"com.js","com.dujie"})
 public class DubboProviderApplication {
 
     public static void main(String[] args) {
